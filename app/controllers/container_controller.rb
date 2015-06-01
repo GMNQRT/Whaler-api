f9f9801b49cdc0441856346877e3ced50c126f40
Balcerek <quentin.balcerek@gmail.com>
2015-06-01 23:25:57 +0200

    list view container

--- app/controllers/container_controller.rb
+++ app/controllers/container_controller.rb
@@ -2,7 +2,11 @@ require 'docker'
 
 class ContainerController < ApplicationController
   respond_to :json
-  def list
+  def index
     respond_with Docker::Container.all(:all => true)
+  end
+
+  def show
+    respond_with Docker::Container.get(params[:id])
   end
 end
