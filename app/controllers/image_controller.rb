0579c38c2763511b0033a17ecbfed486cfb23fdb
nicolas miannay <nicolas.miannay@gmail.com>
2015-06-02 22:41:51 +0200

    Change user and password for database

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,5 +1,5 @@
 class ImageController < ApplicationController
-
+  skip_before_action :verify_authenticity_token
   respond_to :json
   def index
     respond_with Docker::Image.all
@@ -7,6 +7,13 @@ class ImageController < ApplicationController
 
   def show
     respond_with Docker::Image.get(params[:id])
+  end
+
+  def create
+    image = Docker::Image.create('fromImage' => params[:fromImage])
+    image.tag('repo' => 'base2', 'force' => true)
+    image.save
+    respond_with image
   end
 
   def history
