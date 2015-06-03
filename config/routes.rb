4182dd2473a5b462a57b676cf2d1828ae264ec30
Clor <clor.matthieu.tl@gmail.com>
2015-06-04 00:24:22 +0200

    database fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { sessions: "users/sessions" }
 
+  resources :admin_registrations
+
   get 'home/index'
 
   resources :image do
