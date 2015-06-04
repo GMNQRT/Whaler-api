14cd2f93b660e3120142886fb5d96177244dc601
nicolas miannay <nicolas.miannay@gmail.com>
2015-06-05 00:47:42 +0200

    Return refreshed container after basic actions on container

--- app/controllers/container_controller.rb
+++ app/controllers/container_controller.rb
@@ -7,20 +7,7 @@ class ContainerController < ApplicationController
     @Containers = Docker::Container.all(:all => true)
     @Containers.each do |item|
       @Container = Docker::Container.get(item.id)
-      @stateNumber = 0
-      if @Container.info['State']['ExitCode'] == -1
-        @stateNumber = -1;
-      end
-      if @Container.info['State']['Dead']
-        @stateNumber = 1;
-      end
-      if @Container.info['State']['Running']
-        @stateNumber = 2;
-      end
-      if @Container.info['State']['Paused']
-        @stateNumber = 3;
-      end
-      @Container.info['State']['stateNumber'] = @stateNumber
+      # @Container.info['State']['stateNumber'] = @stateNumber
       item.info['State'] = @Container.info['State']
       # stateNumber -1 = erreur
       # stateNumber 1 = mort ?!
@@ -59,27 +46,40 @@ class ContainerController < ApplicationController
   def start
     @Container = Docker::Container.get(params[:id])
     if @Container.info['State']['Paused']
-      @Container.unpause
+      @Container.unpause!
+      @Container.info['State']['Paused'] = false
     end
-    respond_with @Container.start
+    @Container.start!
+    @Container.info['State']['Running'] = true
+
+    respond_with @Container
   end
 
   def stop
     @Container = Docker::Container.get(params[:id])
     if @Container.info['State']['Paused']
       @Container.unpause
+      @Container.info['State']['Paused'] = false
     end
-    respond_with @Container.stop
+    @Container.stop!
+    @Container.info['State']['Running'] = false
+
+
+    respond_with @Container
   end
 
   def pause
     @Container = Docker::Container.get(params[:id])
-    respond_with @Container.pause
+    @Container.pause!
+    @Container.info['State']['Paused'] = true
+    respond_with @Container
   end
 
   def unpause
     @Container = Docker::Container.get(params[:id])
-    respond_with @Container.unpause
+    @Container.unpause!
+    @Container.info['State']['Paused'] = false
+    respond_with @Container
   end
 
   def restart
