96276960d32da406c7c5455705f2278fe3ed7f1d
Balcerek <quentin.balcerek@gmail.com>
2015-06-04 17:04:38 +0200

    update action for containers

--- app/controllers/container_controller.rb
+++ app/controllers/container_controller.rb
@@ -1,9 +1,34 @@
 require 'docker'
+require 'json'
 
 class ContainerController < ApplicationController
   respond_to :json
   def index
-    respond_with Docker::Container.all(:all => true)
+    @Containers = Docker::Container.all(:all => true)
+    @Containers.each do |item|
+      @Container = Docker::Container.get(item.id)
+      @stateNumber = 0
+      if @Container.info['State']['ExitCode'] == -1
+        @stateNumber = -1;
+      end
+      if @Container.info['State']['Dead']
+        @stateNumber = 1;
+      end
+      if @Container.info['State']['Running']
+        @stateNumber = 2;
+      end
+      if @Container.info['State']['Paused']
+        @stateNumber = 3;
+      end
+      @Container.info['State']['stateNumber'] = @stateNumber
+      item.info['State'] = @Container.info['State']
+      # stateNumber -1 = erreur
+      # stateNumber 1 = mort ?!
+      # stateNumber 0 = stop
+      # stateNumber 2 = start
+      # stateNumber 3 = pause
+    end
+    respond_with @Containers
   end
 
   def show
@@ -12,11 +37,17 @@ class ContainerController < ApplicationController
 
   def start
     @Container = Docker::Container.get(params[:id])
+    if @Container.info['State']['Paused']
+      @Container.unpause
+    end
     respond_with @Container.start
   end
 
   def stop
     @Container = Docker::Container.get(params[:id])
+    if @Container.info['State']['Paused']
+      @Container.unpause
+    end
     respond_with @Container.stop
   end
 
