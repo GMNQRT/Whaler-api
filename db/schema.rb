4d1792a755112741611d803af71088e7541cedf4
lordxase <quentin.balcerek@gmail.com>
2015-06-25 11:24:11 +0200

    passage sur sqlite3 - bin/rake db:migrate RAILS_ENV=development - rake db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -14,30 +14,30 @@
 ActiveRecord::Schema.define(version: 20150603214848) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255,   default: "", null: false
-    t.string   "encrypted_password",     limit: 255,   default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,     default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "authentication_token",   limit: 255
-    t.string   "first_name",             limit: 255
-    t.string   "last_name",              limit: 255
-    t.string   "role",                   limit: 255
-    t.string   "skype",                  limit: 255
-    t.string   "linkedin",               limit: 255
-    t.string   "twitter",                limit: 255
-    t.string   "website",                limit: 255
-    t.text     "bio",                    limit: 65535
+    t.string   "authentication_token"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "role"
+    t.string   "skype"
+    t.string   "linkedin"
+    t.string   "twitter"
+    t.string   "website"
+    t.text     "bio"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
