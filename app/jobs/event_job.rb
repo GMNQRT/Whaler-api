0be68d9e731944bd92b85a0f8570f9c777209dd1
Nicolas MIANNAY <nicolas.miannay@gmail.com>
2015-09-26 16:49:31 +0000

    Handle case where container is removed

--- app/jobs/event_job.rb
+++ app/jobs/event_job.rb
@@ -2,8 +2,8 @@ class EventJob < ActiveJob::Base
   queue_as :default
 
   def self.perform
-    Docker::Event.stream do |event|
-      WebsocketRails[:container].trigger :event, event: event, container: Docker::Container.get(event.id)
+    Docker::Event.stream(filters: { event: [:attach, :commit, :copy, :create, :destroy, :die, :exec_create, :exec_start, :export, :kill, :oom, :pause, :rename, :restart, :start, :stop, :top, :unpause] }.to_json) do |event|
+      WebsocketRails[:container].trigger :event, event: event, container: (Docker::Container.get(event.id) rescue nil)
     end
   rescue
     retry
