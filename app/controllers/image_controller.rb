3057691af4b5e35fff4682b3a668831d48132fd8
Nicolas MIANNAY <nicolas.miannay@gmail.com>
2016-04-03 22:29:12 +0000

    Add tags action for image

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -15,14 +15,22 @@ class ImageController < ApplicationController
     respond_with params[:term] ? Docker::Image.search(term: params[:term]) : []
   end
 
+  def tags
+    if params[:image]
+      respond_with JSON.load(open("https://registry.hub.docker.com/v1/repositories/#{params[:image]}/tags"))
+    else
+      respond_with []
+    end
+  end
+
   def destroy
     @Image = Docker::Image.get(params[:id])
     respond_with @Image.delete(:force => true)
   end
 
   def run
-    @image     = Docker::Image.create(fromImage: params[:id])
-    @container = Docker::Container.create(Image: params[:id])
+    @image     = Docker::Image.create(fromImage: "#{params[:id]}:#{params[:tag]}")
+    @container = Docker::Container.create(Image: "#{params[:id]}:#{params[:tag]}")
 
     @container.start
     respond_to do |format|
