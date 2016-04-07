466344de17da1a400d72d5b3b445e206a186374d
Nicolas MIANNAY <nicolas.miannay@gmail.com>
2016-04-07 22:31:05 +0000

    Render json object in admin_registrations_controlle instead of notice

--- app/controllers/admin_registrations_controller.rb
+++ app/controllers/admin_registrations_controller.rb
@@ -1,6 +1,6 @@
 class AdminRegistrationsController < ApplicationController
   before_action :authenticate_user_from_token!
-  
+
   def index
     @users = User.all
     render json: @users
@@ -13,9 +13,8 @@ class AdminRegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      flash[:notice] = "Successfully created User."
-      render json: { notice: flash[:notice] }
+    if @user.save!
+      render json: @user
     else
       render json: { error: flash[:error] }
     end
@@ -23,11 +22,9 @@ class AdminRegistrationsController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    params[:user].delete(:password) if params[:user][:password].blank?
-    params[:user].delete(:password_confirmation) if params[:user][:password_confirmation].blank?
+
     if @user.update!(user_params)
-      flash[:notice] = "Successfully updated User."
-      render json: { notice: flash[:notice] }
+      render json: @user
     else
       render json: { error: flash[:error] }
     end
@@ -44,6 +41,6 @@ class AdminRegistrationsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :role, :email, :password, :created_at, :updated_at, :password_confirmation)
+    params.require(:admin_registration).permit(:first_name, :last_name, :role, :email, :password, :created_at, :updated_at, :password_confirmation)
   end
 end
